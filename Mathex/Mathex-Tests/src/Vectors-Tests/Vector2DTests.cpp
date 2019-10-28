07b0df0dee6297b5ce5fd2efd7e40d80483bccbd
Ori Lazar <36171247+Kney-Delach@users.noreply.github.com>
2019-10-28 17:31:31 +0000

    Vector2D unit tests passing.
    Added some more unit tests for the Vector2D, all are passing.

--- Mathex/Mathex-Tests/src/Vectors-Tests/Vector2DTests.cpp
+++ Mathex/Mathex-Tests/src/Vectors-Tests/Vector2DTests.cpp
@@ -27,3 +27,54 @@ TEST(Vector_2D_Tests, Constructors)
 	ASSERT_EQ(vec2b.x, 1) << "Non-Default constructor results in wrong x value.";
 	ASSERT_EQ(vec2b.y, -1) << "Non-Default constructor results in wrong y value.";
 }
+
+TEST(Vector_2D_Tests, Clear)
+{
+	Vector2D vec2(1, 1);
+	vec2.Clear();
+	ASSERT_EQ(vec2.x, 0) << "Clear function results in wrong x value.";
+	ASSERT_EQ(vec2.y, 0) << "Clear function results in wrong y value.";
+}
+
+TEST(Vector_2D_Tests, Accessors)
+{
+	Vector2D vec2(1, 2);
+	ASSERT_EQ(vec2[0], 1) << "Index 0 accessor results in wrong accessed index.";
+	ASSERT_EQ(vec2[1], 2) << "Index 1 accessor results in wrong accessed index.";
+}
+
+TEST(Vector_2D_Tests, Scalar_Multiplication)
+{
+	Vector2D vec2(1, 1);
+	const float scalar = 2;
+	vec2 *= scalar; 
+	ASSERT_EQ(vec2.x, 2) << "Scalar multiplication results in wrong x value.";
+	ASSERT_EQ(vec2.y, 2) << "Scalar multiplication results in wrong y value.";
+}
+
+TEST(Vector_2D_Tests, Scalar_Division)
+{
+	Vector2D vec2(4, 4);
+	const float scalar = 2;
+	vec2 /= scalar;
+	ASSERT_EQ(vec2.x, 2) << "Scalar division results in wrong x value.";
+	ASSERT_EQ(vec2.y, 2) << "Scalar division results in wrong y value.";
+}
+
+TEST(Vector_2D_Tests, Vector_Addition)
+{
+	Vector2D vec2(1, 1);
+	const Vector2D vec2b(2, 2);
+	vec2 += vec2b;
+	ASSERT_EQ(vec2.x, 3) << "Vector addition results in wrong x value.";
+	ASSERT_EQ(vec2.y, 3) << "Vector addition results in wrong y value.";
+}
+
+TEST(Vector_2D_Tests, Vector_Subtraction)
+{
+	Vector2D vec2(3, 3);
+	const Vector2D vec2b(2, 2);
+	vec2 -= vec2b;
+	ASSERT_EQ(vec2.x, 1) << "Vector subtraction results in wrong x value.";
+	ASSERT_EQ(vec2.y, 1) << "Vector subtraction results in wrong y value.";
+}
